04c67a6b227bcf31b97289bdda7af5f90d1933e7
xcloudx12345 <40192522+xcloudx12345@users.noreply.github.com>
2023-02-07 16:32:01 +0700

    Update responses.py

--- src/responses.py
+++ src/responses.py
@@ -1,27 +1,34 @@
+# Import the necessary libraries
 import os
 from revChatGPT.Official import AsyncChatbot
 import json
 
-
+# Get config data as dictionary
 def get_config() -> dict:
-    # get config.json path
+    # Get the path for the config.json file using ospath library
     config_dir = os.path.abspath(__file__ + "/../../")
     config_name = 'config.json'
     config_path = os.path.join(config_dir, config_name)
 
+    # Load the config.json file as a dictionary
     with open(config_path, 'r') as f:
         config = json.load(f)
 
+    # Return config dict 
     return config
 
-
+# Call the get_config function to retrieve config data as a dict
 config = get_config()
-#chatbot = Chatbot(api_key=config['openAI_key'])
+
+# Initialize the AsyncChatbot class with API key from either config or environment variable
 chatbot = AsyncChatbot(api_key=os.environ['openAI_key'])
 
-
+# handle_response function returns response from chatbot to a message
 async def handle_response(message) -> str:
+    # Get response from chatbot
     response = await chatbot.ask(message, 0)
+    # Select the first choice from response
     responseMessage = response["choices"][0]["text"]
 
+    # Return response as string
     return responseMessage
